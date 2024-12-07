1e12248ab4e8a5ff2ae17360642b7ddaf539814b
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-07 15:34:37 +0800

    test

--- backend/Login/home.php
+++ backend/Login/home.php
@@ -1,15 +1,15 @@
 <?php
     session_start();
-
+    include '/backend/server.php';
     // Check if the user is logged in and has a valid role (Admin or Cashier)
     if (!isset($_SESSION['user']['role']) || 
         ($_SESSION['user']['role'] !== 'Admin' && $_SESSION['user']['role'] !== 'Cashier')) {
         // Redirect to login if not logged in or not authorized
-        header('Location: /Kape_Cinco/frontend/Login/login.html?redirect=Login/home.php');
+        header('Location: /frontend/Login/login.html?redirect=Login/home.php');
         exit;
     }
 
     // Include the home.html content if the user is valid
-    header('Location: /Kape_Cinco/frontend/Home/home.html');
+    header('Location: /frontend/Home/home.html');
     exit;
 ?>
