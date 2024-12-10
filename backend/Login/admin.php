721c938b3ed10974f269b633ee8c191d607ba75b
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-10 15:49:35 +0800

    test

--- backend/Login/admin.php
+++ backend/Login/admin.php
@@ -3,25 +3,25 @@
     include '../server.php';
     // Check if the user is logged in
     if (!isset($_SESSION['user']['role'])) {
-        header(`Location: /frontend/Login/login.html?redirect=Login/admin.php`);
+        header('Location: /frontend/Login/login.html?redirect=Login/admin.php');
         exit;
     }
 
     // Check if the user role is not Admin
     if ($_SESSION['user']['role'] === 'Cashier') {
         echo "<script>
-                alert(`Access denied: You do not have permission to view this content.`);
+                alert('Access denied: You do not have permission to view this content.');
                 window.location.href = '/frontend/Home/home.html';
             </script>";
         exit; 
     }
 
     if ($_SESSION['user']['role'] !== 'Admin') {
-        header(`Location: /frontend/Login/login.html?redirect=Login/admin.php`);
+        header('Location: /frontend/Login/login.html?redirect=Login/admin.php');
         exit;
     }
 
     // Redirect to the admin page
-    header(`Location: /frontend/Admin/admin.html`);
+    header('Location: /frontend/Admin/admin.html');
     exit;
 ?>
