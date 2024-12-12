5bce178eb1c88539c93d60a407d377bd8c1b4030
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-13 06:25:25 +0800

    try

--- backend/Login/login.php
+++ backend/Login/login.php
@@ -15,7 +15,6 @@
             $stmt->execute();
             $result = $stmt->get_result();
 
-            echo $username;
             
             if ($result->num_rows > 0) {
                 $user = $result->fetch_assoc();
