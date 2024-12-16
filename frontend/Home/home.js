30e24c468e76ba40471c27176daf052ff0bf694a
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-16 16:16:14 +0800

    test

--- frontend/Home/home.js
+++ frontend/Home/home.js
@@ -416,7 +416,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     confirmRemit.addEventListener('click', () => {
         remittance();
-        logout();
+        //logout();
         document.body.classList.remove('hidden');
     })
 
