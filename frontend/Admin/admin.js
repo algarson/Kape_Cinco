249b2752c607836a2d749457c7bd57e9e8f70f43
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-17 01:35:39 +0800

    test

--- frontend/Admin/admin.js
+++ frontend/Admin/admin.js
@@ -1018,7 +1018,7 @@ async function getSummaryLog() {
         if (data.success) {
             // Example: Update the UI with the total sales
             const totalSales = data.data.total_sales || 0;
-            document.getElementById("sale-summary").innerText = `Total Sales: PHP ${totalSales.toFixed(2)}`;
+            document.getElementById("sale-summary").textContent = ` PHP ${totalSales.toFixed(2)}`;
         } else if (data.error) {
             // Handle errors sent from the backend
             alert(data.error);
