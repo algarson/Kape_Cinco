fcbd4abf9fb9cdb22e4ad2c61a0419b9384b527f
Allen Luis Alvarez <allen.luis626@gmail.com>
2024-12-17 22:02:31 +0800

    test

--- frontend/Admin/admin.js
+++ frontend/Admin/admin.js
@@ -1187,7 +1187,7 @@ function openSummaryLogModal() {
     const modal = document.getElementById("summaryLogModal");
     const modal2 = document.getElementById("summarylogmodaldate");
     modal.style.display = "block";
-    modal2.style.display = "block";
+    modal2.style.display = "none";
 }
 
 // Function to close the Summary Log Modal
